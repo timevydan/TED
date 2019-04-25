85cfdc29ca2a0192ae34f52affb1372fe7ff2925
Dan Le <Danhuyle508@gmail.com>
2019-04-25 16:45:11 -0700

    merged

--- ted/urls.py
+++ ted/urls.py
@@ -3,7 +3,6 @@ from .views import FaceListView, FaceCreateView, PictureListView, PictureCreateV
 
 urlpatterns = [
     path('face_list', FaceListView.as_view(), name='face_list'),
-    path('face_list/<int:pk>/delete/', DeleteFaceView.as_view(), name='face_delete'),
     path('face/add', FaceCreateView.as_view(), name='face_add'),
     path('<int:pk>', PictureListView.as_view(), name='urls'),
     path('<int:pk>/delete/', DeletePictureView.as_view(), name='picture_delete'),
